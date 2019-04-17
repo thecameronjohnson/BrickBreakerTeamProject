e5daed8e2f0c6ee5711a5e480b3246735d9050a9
Brendon Roy <brendonroy@hotmail.ca>
2019-04-17 09:33:17 -0400

    XML PROGRESS

--- BrickBreaker/Block.cs
+++ BrickBreaker/Block.cs
@@ -15,11 +15,11 @@ namespace BrickBreaker
         public int x;
         public int y; 
         public int hp;
-        public Color colour;
+        public string colour;
 
         public static Random rand = new Random();
 
-        public Block(int _x, int _y, int _hp, Color _colour)
+        public Block(int _x, int _y, int _hp, string _colour)
         {
             x = _x;
             y = _y;
--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -1,7 +1,7 @@
 ﻿/*  Created by: Steven HL
  *  Project: Brick Breaker
  *  Date: Tuesday, April 4th
- */ 
+ */
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,200 +12,232 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
+using System.Xml;
 
 namespace BrickBreaker
 {
-    public partial class GameScreen : UserControl
-    {
-        #region global values
+	public partial class GameScreen : UserControl
+	{
+		#region global values
 
-        //player1 button control keys - DO NOT CHANGE
-        Boolean leftArrowDown, rightArrowDown;
+		//player1 button control keys - DO NOT CHANGE
+		Boolean leftArrowDown, rightArrowDown;
 
-        // Game values
-        int lives;
+		// Game values
+		int lives;
 
-        // Paddle and Ball objects
-        Paddle paddle;
-        Ball ball;
+		// Paddle and Ball objects
+		Paddle paddle;
+		Ball ball;
 
-        // list of all blocks for current level
-        List<Block> blocks = new List<Block>();
+		// list of all blocks for current level
+		List<Block> blocks = new List<Block>();
 
-        // Brushes
-        SolidBrush paddleBrush = new SolidBrush(Color.White);
-        SolidBrush ballBrush = new SolidBrush(Color.White);
-        SolidBrush blockBrush = new SolidBrush(Color.Red);
+		// Brushes
+		SolidBrush paddleBrush = new SolidBrush(Color.White);
+		SolidBrush ballBrush = new SolidBrush(Color.White);
+		SolidBrush blockBrush = new SolidBrush(Color.Red);
 
-        #endregion
+		#endregion
 
-        public GameScreen()
-        {
-            InitializeComponent();
-            OnStart();
-        }
+		public GameScreen()
+		{
+			InitializeComponent();
+			OnStart();
+		}
 
 
-        public void OnStart()
-        {
-            //set life counter
-            lives = 3;
+		public void OnStart()
+		{
+			//set life counter
+			lives = 3;
 
-            //set all button presses to false.
-            leftArrowDown = rightArrowDown = false;
+			//set all button presses to false.
+			leftArrowDown = rightArrowDown = false;
 
-            // setup starting paddle values and create paddle object
-            int paddleWidth = 80;
-            int paddleHeight = 20;
-            int paddleX = ((this.Width / 2) - (paddleWidth / 2));
-            int paddleY = (this.Height - paddleHeight) - 60;
-            int paddleSpeed = 8;
-            paddle = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight, paddleSpeed, Color.White);
+			// setup starting paddle values and create paddle object
+			int paddleWidth = 80;
+			int paddleHeight = 20;
+			int paddleX = ((this.Width / 2) - (paddleWidth / 2));
+			int paddleY = (this.Height - paddleHeight) - 60;
+			int paddleSpeed = 8;
+			paddle = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight, paddleSpeed, Color.White);
 
-            // setup starting ball values
-            int ballX = this.Width / 2 - 10;
-            int ballY = this.Height - paddle.height - 80;
+			// setup starting ball values
+			int ballX = this.Width / 2 - 10;
+			int ballY = this.Height - paddle.height - 80;
 
-            // Creates a new ball
-            int xSpeed = 6;
-            int ySpeed = 6;
-            int ballSize = 20;
-            ball = new Ball(ballX, ballY, xSpeed, ySpeed, ballSize);
+			// Creates a new ball
+			int xSpeed = 6;
+			int ySpeed = 6;
+			int ballSize = 20;
+			ball = new Ball(ballX, ballY, xSpeed, ySpeed, ballSize);
 
-            #region Creates blocks for generic level. Need to replace with code that loads levels.
+			#region Creates blocks for generic level. Need to replace with code that loads levels.
 
-            blocks.Clear();
-            int x = 10;
+			blocks.Clear();
+			int x = 10;
 
-            while (blocks.Count < 12)
-            {
-                x += 57;
-                Block b1 = new Block(x, 10, 1, Color.White);
-                blocks.Add(b1);
-            }
+			while (blocks.Count < 12)
+			{
+				x += 57;
+				Block b1 = new Block(x, 10, 1, "Blue");
+				blocks.Add(b1);
+			}
 
-            #endregion
+			#endregion
 
-            // start the game engine loop
-            gameTimer.Enabled = true;
-        }
+			// start the game engine loop
+			gameTimer.Enabled = true;
+		}
 
-        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
-        {
-            //player 1 button presses
-            switch (e.KeyCode)
-            {
-                case Keys.Left:
-                    leftArrowDown = true;
-                    break;
-                case Keys.Right:
-                    rightArrowDown = true;
-                    break;
-                default:
-                    break;
-            }
-        }
+		private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+		{
+			//player 1 button presses
+			switch (e.KeyCode)
+			{
+				case Keys.Left:
+					leftArrowDown = true;
+					break;
+				case Keys.Right:
+					rightArrowDown = true;
+					break;
+				default:
+					break;
+			}
+		}
 
-        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
-        {
-            //player 1 button releases
-            switch (e.KeyCode)
-            {
-                case Keys.Left:
-                    leftArrowDown = false;
-                    break;
-                case Keys.Right:
-                    rightArrowDown = false;
-                    break;
-                default:
-                    break;
-            }
-        }
+		private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+		{
+			//player 1 button releases
+			switch (e.KeyCode)
+			{
+				case Keys.Left:
+					leftArrowDown = false;
+					break;
+				case Keys.Right:
+					rightArrowDown = false;
+					break;
+				default:
+					break;
+			}
+		}
 
-        private void gameTimer_Tick(object sender, EventArgs e)
-        {
-            // Move the paddle
-            if (leftArrowDown && paddle.x > 0)
-            {
-                paddle.Move("left");
-            }
-            if (rightArrowDown && paddle.x < (this.Width - paddle.width))
-            {
-                paddle.Move("right");
-            }
+		private void gameTimer_Tick(object sender, EventArgs e)
+		{
+			// Move the paddle
+			if (leftArrowDown && paddle.x > 0)
+			{
+				paddle.Move("left");
+			}
+			if (rightArrowDown && paddle.x < (this.Width - paddle.width))
+			{
+				paddle.Move("right");
+			}
 
-            // Move ball
-            ball.Move();
+			// Move ball
+			ball.Move();
 
-            // Check for collision with top and side walls
-            ball.WallCollision(this);
+			// Check for collision with top and side walls
+			ball.WallCollision(this);
 
-            // Check for ball hitting bottom of screen
-            if (ball.BottomCollision(this))
-            {
-                lives--;
+			// Check for ball hitting bottom of screen
+			if (ball.BottomCollision(this))
+			{
+				lives--;
 
-                // Moves the ball back to origin
-                ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
-                ball.y = (this.Height - paddle.height) - 85;
+				// Moves the ball back to origin
+				ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
+				ball.y = (this.Height - paddle.height) - 85;
 
-                if (lives == 0)
-                {
-                    gameTimer.Enabled = false;
-                    OnEnd();
-                }
-            }
+				if (lives == 0)
+				{
+					gameTimer.Enabled = false;
+					OnEnd();
+				}
+			}
 
-            // Check for collision of ball with paddle, (incl. paddle movement)
-            ball.PaddleCollision(paddle, leftArrowDown, rightArrowDown);
+			// Check for collision of ball with paddle, (incl. paddle movement)
+			ball.PaddleCollision(paddle, leftArrowDown, rightArrowDown);
 
-            // Check if ball has collided with any blocks
-            foreach (Block b in blocks)
-            {
-                if (ball.BlockCollision(b))
-                {
-                    blocks.Remove(b);
+			// Check if ball has collided with any blocks
+			foreach (Block b in blocks)
+			{
+				if (ball.BlockCollision(b))
+				{
+					blocks.Remove(b);
 
-                    if (blocks.Count == 0)
-                    {
-                        gameTimer.Enabled = false;
-                        OnEnd();
-                    }
+					if (blocks.Count == 0)
+					{
+						gameTimer.Enabled = false;
+						OnEnd();
+					}
 
-                    break;
-                }
-            }
+					break;
+				}
+			}
 
-            //redraw the screen
-            Refresh();
-        }
+			//redraw the screen
+			Refresh();
+		}
 
-        public void OnEnd()
-        {
-            // Goes to the game over screen
-            Form form = this.FindForm();
-            MenuScreen ps = new MenuScreen();
-            
-            ps.Location = new Point((form.Width - ps.Width) / 2, (form.Height - ps.Height) / 2);
+		public void OnEnd()
+		{
+			// Goes to the game over screen
+			Form form = this.FindForm();
+			MenuScreen ps = new MenuScreen();
 
-            form.Controls.Add(ps);
-            form.Controls.Remove(this);
-        }
+			ps.Location = new Point((form.Width - ps.Width) / 2, (form.Height - ps.Height) / 2);
 
-        public void GameScreen_Paint(object sender, PaintEventArgs e)
-        {
-            // Draws paddle
-            paddleBrush.Color = paddle.colour;
-            e.Graphics.FillRectangle(paddleBrush, paddle.x, paddle.y, paddle.width, paddle.height);
+			form.Controls.Add(ps);
+			form.Controls.Remove(this);
+		}
 
-            // Draws blocks
-            foreach (Block b in blocks)
-            {
-                e.Graphics.FillRectangle(blockBrush, b.x, b.y, b.width, b.height);
-            }
+		public void GameScreen_Paint(object sender, PaintEventArgs e)
+		{
+			// Draws paddle
+			paddleBrush.Color = paddle.colour;
+			e.Graphics.FillRectangle(paddleBrush, paddle.x, paddle.y, paddle.width, paddle.height);
 
-            // Draws ball
-            e.Graphics.FillRectangle(ballBrush, ball.x, ball.y, ball.size, ball.size);
-        }
-    }
+			// Draws blocks
+			foreach (Block b in blocks)
+			{
+				e.Graphics.FillRectangle(blockBrush, b.x, b.y, b.width, b.height);
+			}
+
+			// Draws ball
+			e.Graphics.FillRectangle(ballBrush, ball.x, ball.y, ball.size, ball.size);
+		}
+
+		private void loadXML()
+		{
+			int hp = 0, x = 0, y = 0;
+			string colour = null;
+			XmlReader reader = XmlReader.Create("");
+
+			while (reader.Read())
+			{
+				if (reader.NodeType == XmlNodeType.Text)
+				{
+					reader.ReadToNextSibling("x");
+					x = reader.ReadContentAsInt();
+
+					reader.ReadToNextSibling("y");
+					y = reader.ReadContentAsInt();
+
+					reader.ReadToNextSibling("hp");
+					hp = reader.ReadContentAsInt();
+
+					reader.ReadToNextSibling("colour");
+					colour = reader.ReadString();
+
+					reader.ReadToNextSibling("");
+
+					Block b = new Block(x, y, hp, colour);
+					blocks.Add(b);
+				}
+				reader.Close();
+			}
+		}
+	}
 }
