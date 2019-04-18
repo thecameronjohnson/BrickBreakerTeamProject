51fc2941eaeda443ee2077a9be95e97a54173b90
garrsaun253 <garrsaun253@SCS-D-360QHY>
2019-04-18 08:59:46 -0400

    "You are not me" - David Branch

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -241,6 +241,22 @@ namespace BrickBreaker
             // Draws blocks
             foreach (Block b in blocks)
             {
+                switch (b.hp)
+                {
+                    case 1:
+                        b.colour = Color.Red;
+                        break;
+                    case 2:
+                        b.colour = Color.Yellow;
+                        break;
+                    case 3:
+                        b.colour = Color.Green;
+                        break;
+                    case 4:
+                        b.colour = Color.Cyan;
+                        break;
+                }
+
                 SolidBrush blockBrush = new SolidBrush(b.colour);
                 e.Graphics.FillRectangle(blockBrush, b.x, b.y, b.width, b.height);
             }
