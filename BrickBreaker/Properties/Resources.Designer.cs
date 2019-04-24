d8a82085ea6053afbdc8905c25e41013414d56b5
Cjeran <cierdieb649@ed.amdsb.ca>
2019-04-24 09:33:38 -0400

    E

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -92,9 +92,10 @@ namespace BrickBreaker.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
+        ///
         ///&lt;levels&gt;
         ///&lt;level number =&quot;1&quot; name =&quot;Level 1&quot;&gt;
-        ///  &lt;brick&gt;
+        ///  &lt;brick x =&quot;69&quot; y =&quot;47&quot; hp =&quot;1&quot;&gt;
         ///    &lt;x&gt;69&lt;/x&gt;
         ///    &lt;y&gt;47&lt;/y&gt;
         ///    &lt;hp&gt;1&lt;/hp&gt;
@@ -122,10 +123,7 @@ namespace BrickBreaker.Properties {
         ///  &lt;brick&gt;
         ///    &lt;x&gt;107&lt;/x&gt;
         ///    &lt;y&gt;137&lt;/y&gt;
-        ///    &lt;hp&gt;2&lt;/hp&gt;
-        ///  &lt;/brick&gt;
-        ///
-        ///  &lt;b [rest of string was truncated]&quot;;.
+        ///    &lt;hp&gt; [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Levels {
             get {
@@ -139,6 +137,16 @@ namespace BrickBreaker.Properties {
         internal static System.Drawing.Bitmap purpBrick {
             get {
                 object obj = ResourceManager.GetObject("purpBrick", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap purpbutton {
+            get {
+                object obj = ResourceManager.GetObject("purpbutton", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
