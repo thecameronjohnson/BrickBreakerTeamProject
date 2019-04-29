14f351bcf33f187c74be3738974add7b87f457a8
cam johnson <the.camjohnson@gmail.com>
2019-04-29 09:15:10 -0400

    yes

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -27,6 +27,7 @@ namespace BrickBreaker
         // Game values
         string level, levelName;
         public static int lives, score, scoreMult;
+        public static int powerupSpeed = 5;
         public static double lastPower = 0;
         public static int bSpeedMult = 1;
         public static int pSpeedMult = 1;
@@ -208,13 +209,14 @@ namespace BrickBreaker
                         b.size = 20;
 
                         Refresh();
-                        Thread.Sleep(2000);
+                        
 
                         if (lives == 0)
                         {
                             gameTimer.Enabled = false;
                             OnEnd();
                         }
+                        Thread.Sleep(2000);
                     }
                 }
             }
@@ -266,9 +268,11 @@ namespace BrickBreaker
                         if (scoreint > lastPower)
                         {
                             lastPower = scoreint;
+                            NumberGen();
+                            int powertype = powerValue;
 
-                            //power = new PowerUp(blockSize / 2 + blockX, blockY, );
-                            //powers.Add(power);
+                            PowerUp power = new PowerUp(blockSize / 2 + blockX, blockY, powerupSpeed, 15, powertype);
+                            powers.Add(power);
                         }                        
 
                     }
@@ -351,6 +355,7 @@ namespace BrickBreaker
         {          
             powerValue = randGen.Next(1, 5);
         }
+
         public void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             // Draws paddle
@@ -362,6 +367,13 @@ namespace BrickBreaker
             {
                 SolidBrush blockBrush = new SolidBrush(b.UpdateColour());
                 e.Graphics.FillRectangle(blockBrush, b.x, b.y, b.width, b.height);
+            }
+
+            // Draws powerups
+            foreach (PowerUp p in powers)
+            {
+                SolidBrush powerBrush = new SolidBrush(p.UpdateColour());
+                e.Graphics.FillRectangle(powerBrush, p.x, p.y, p.size, p.size);
             }
 
             // Draws ball
