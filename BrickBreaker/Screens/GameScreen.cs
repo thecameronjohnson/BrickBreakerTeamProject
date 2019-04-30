f0536c5a69f8e73eecfbb95d1761759545af41c0
Cjeran <cierdieb649@ed.amdsb.ca>
2019-04-30 09:30:10 -0400

    Added Holding instead of Thread.Sleep();

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -182,7 +182,7 @@ namespace BrickBreaker
             if (holding)
             {
                 ballList[0].x = (paddle.x + (paddle.width / 2)) - ballList[0].size / 2;
-                ballList[0].y = paddle.y - 85;
+                ballList[0].y = (this.Height - paddle.height) - 85;
 
                 //If space bar pressed, release ball
                 if (spaceKeyDown)
@@ -229,14 +229,13 @@ namespace BrickBreaker
                         b.size = 20;
 
                         Refresh();
-                        
+                        holding = true;
 
                         if (lives == 0)
                         {
                             gameTimer.Enabled = false;
                             OnEnd();
                         }
-                        Thread.Sleep(2000);
                     }
                 }
             }
