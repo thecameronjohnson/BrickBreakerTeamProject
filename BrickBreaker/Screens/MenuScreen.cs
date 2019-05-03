3331419bd8ec8804ebdba82297d5d26a99ab9b48
Loren <lorewolf686@ed.amdsb.ca>
2019-05-03 09:20:11 -0400

    name sceen allign

--- BrickBreaker/Screens/MenuScreen.cs
+++ BrickBreaker/Screens/MenuScreen.cs
@@ -16,7 +16,7 @@ namespace BrickBreaker
         public MenuScreen()
         {
             InitializeComponent();
-            //lorenAllignment();
+            lorenAllignment();
         }
 
         private void exitButton_Click(object sender, EventArgs e)
@@ -34,13 +34,12 @@ namespace BrickBreaker
 
         private void lorenAllignment()
         {
-            playButton.Location = new Point((this.Width - playButton.Width) / 2, 240);
-            howButton.Location = new Point((this.Width - howButton.Width) / 2, 320); 
+            playButton.Location = new Point((this.Width - playButton.Width) / 2, 240);            
             exitButton.Location = new Point((this.Width - exitButton.Width) / 2, 400);
             title.Location = new Point((this.Width - title.Width) / 2, 130);
         }
 
-		/*private void showDebug_CheckedChanged(object sender, EventArgs e)
+		private void showDebug_CheckedChanged(object sender, EventArgs e)
 		{
 			if (showDebug.Checked == true)
 			{
@@ -52,7 +51,7 @@ namespace BrickBreaker
 				highButton.Visible = false;
 				nameButton.Visible = false;
 			}
-		}*/
+		}
 
 		private void nameButton_Click(object sender, EventArgs e)
 		{
