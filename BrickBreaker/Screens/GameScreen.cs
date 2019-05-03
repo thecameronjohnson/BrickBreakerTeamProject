6cdac2ec80f86da77ff0537697228b09a2ea9b64
Andrew Henderson <andrhend062@ed.amdsb.ca>
2019-05-03 09:26:34 -0400

    yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -89,7 +89,7 @@ namespace BrickBreaker
             int ballSize = 20;
             ball = new Ball(ballX, ballY, xSpeed, ySpeed, ballSize);
             ballList.Add(ball);
-
+            
 
             NewLevel();
 
